5498922081c75cf521e6938d0e81c4c8152499ff
leonardoCavazzani <cavazzanileonardo@gmail.com>
2018-03-15 00:09:46 -0300

    mapping over array and showing than

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View } from 'react-native';
 import AddEntry from './components/AddEntry'
 
+
 export default class App extends React.Component {
   render() {
     return (
---
+++ components/MySlider.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+export default function MySlider (){
+    return(
+        <View>
+            <Text>MySlider</Text>
+        </View>
+    )
+}
